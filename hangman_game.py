17b81cc59c1c1b1f6bc7097dba1b7f0a7ef9873c
julianbarr368 <barraganjulian368@gmail.com>
2022-06-14 16:32:04 -0500

    Primer commit

---
+++ hangman_game.py
@@ -0,0 +1,30 @@
+def hangman_game(letra):
+    word = []
+
+
+
+
+def run():       
+    print("""
+
+                                                       HANGMAN GAME 
+
+    Welcome to the hangman game. 
+
+    You have to guess the word to win  
+
+    Good look !!!!!   😆😆😆
+
+    """)
+
+    letra = input('Escribe una letra:')
+
+    hangman_game(letra)
+
+    
+    
+ 
+
+
+if __name__ == '__main__':
+    run()
