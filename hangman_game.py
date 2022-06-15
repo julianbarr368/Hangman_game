450db5c2d4f0149c4c63edb5f645744ce9c88f55
julianbarr368 <barraganjulian368@gmail.com>
2022-06-15 14:24:46 -0500

    Hangman game

--- hangman_game.py
+++ hangman_game.py
@@ -1,10 +1,64 @@
-def hangman_game(letra):
-    word = []
+from curses.ascii import isalpha
+import random
+
+def get_work():
+    """
+    Toma y guarda una palabra aleatoria del archivo data.txt
+    
+    retorna una palabra en mayuscula
+    """
+    with open('./files/data.txt', 'r', encoding='utf-8') as f:
+        DATA = [i for i in f ]
+
+    word = random.choice(DATA)
+    return word.upper()
 
 
+def hangman_game(word):
+    guess_word = '_' * len(word) - 1 
+    guessed = False
+    guessed_letter = []
+    guessed_words =[]
+    tries = 6
+    print(guess_word)
+    while guessed == False and tries >0:
+        user = input('Please guess a letter of the word: ').upper()
+        if len(user) == 1 and user.isalpha():
+            if user in guessed_letter:
+                print('your already guess the letter ', user)
+            elif user not in word:
+                print(user, ' is not in the word')
+                tries -= 1
+                guessed_letter.appent(user)
+            else:
+                print('Good job ', user , 'is in the word')
+                guessed_letter.appent(user)
+                word_in_list = list(guess_word)
+                indices = [i for i, letter in enumerate(word) if letter == user]
+                for index in indices:
+                    word_in_list[index] = user
+                guess_word = ''.join(word_in_list)
+                if'_' not in guess_word:
+                    guessed = True
+        elif len(user) == len(word) and user.isalpha():
+            if user in guess_word:
+                print('You already guess the word: ', user)
+            elif user != word:
+                print(user,' Is not the word.')
+                tries -= 1
+                guessed_words.append(user)
+            else:
+                guessed= True
+                guess_word = word                     
+        else:
+            print('please enter a letter')
+        print(guess_word)
+    if guessed:
+        print('Congrats, you guess the word, you win!!!')
+    else:
+        print('Sorry, you raw out of try. The word was: ' + word + 'Try again...')
 
-
-def run():       
+def run():
     print("""
 
                                                        HANGMAN GAME 
@@ -16,14 +70,11 @@ def run():
     Good look !!!!!   😆😆😆
 
     """)
-
-    letra = input('Escribe una letra:')
-
-    hangman_game(letra)
-
-    
-    
- 
+    word = get_work()
+    hangman_game(word)
+    while input('Want to play again Y/N').upper() == 'Y':
+         word = get_work()
+         hangman_game(word)
 
 
 if __name__ == '__main__':
